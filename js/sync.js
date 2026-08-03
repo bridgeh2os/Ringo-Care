c1af5dea1d5bc12059255a966a692f1f9fa0a33b
bridgeh2o <37812792+bridgeh2os@users.noreply.github.com>
2026-08-02 19:41:49 -0500

    Set GOOGLE_SCRIPT_URL to the actual script URL

--- js/sync.js
+++ js/sync.js
@@ -8,7 +8,7 @@ For now, it only prepares the structure.
 */
 
 
-const GOOGLE_SCRIPT_URL = "";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyzpKEKilhM8JjIdKAKQRk79qihw1tM5HiLNssnwyYDTw45vetBUWqHQZDH5I4lKuvN/exec";
 
 
 export async function syncMedicationLog(logEntry) {
